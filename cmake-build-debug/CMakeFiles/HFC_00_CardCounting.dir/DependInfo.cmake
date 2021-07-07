9ab5372e481e64b1467421032cf507e15971847e
wrenhainsworth <hainsworthwren@gmail.com>
2021-07-07 22:58:03 +0800

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/HFC_00_CardCounting.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/wren/Data/02-Devprojects/01-C/playground/00-HeadFirstC/HFC-00-CardCounting/main.c" "/home/wren/Data/02-Devprojects/01-C/playground/00-HeadFirstC/HFC-00-CardCounting/cmake-build-debug/CMakeFiles/HFC_00_CardCounting.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/HFC_00_CardCounting.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HFC_00_CardCounting.dir/main.c.o"
+  "HFC_00_CardCounting"
+  "HFC_00_CardCounting.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HFC_00_CardCounting.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
